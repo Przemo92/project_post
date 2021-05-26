d3c8930fdc184a4386f24c9d15d64b90e53c91b5
Przemo <przemek.kapela92@gmail.com>
2021-05-27 01:56:23 +0200

    add Command feature

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -7,12 +7,9 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegisterUserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class RegistrationController extends AbstractController
 {
@@ -22,13 +19,10 @@ class RegistrationController extends AbstractController
     }*/
 
     /**
-     * @Route("/home", name="default", name="home")
+     * @Route("/sign", name="sign")
      */
     public function index(Request $request, UserPasswordEncoderInterface $passwordEncoder)
     {
-        /*$entityManager = $this->getDoctrine()->getManager();
-        $users = $entityManager->getRepository(User::class)->findAll();
-        dump($users);*/
 
         $user = new User();
         $form = $this->createForm(RegisterUserType::class, $user);
@@ -45,25 +39,11 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            return $this->redirectToRoute('home');
+            return $this->redirect("/");
         }
-
 
         return $this->render('registration.html.twig', [
             'form' => $form->createView(),
         ]);
-    }
-    /**
-     * @Route("/login", name="login")
-     */
-    public function login(AuthenticationUtils $authenticationUtils)
-    {
-        $error = $authenticationUtils->getLastAuthenticationError();
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('Security/login.html.twig', array(
-            'last_username' => $lastUsername,
-            'error'         => $error,
-        ));
     }
 }
